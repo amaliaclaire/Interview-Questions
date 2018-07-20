353f434b12a82d7a12a9cbaf217b7dc8746754ea
Amalia Claire Fergin <acfergin@msn.com>
2018-07-20 14:18:57 -0700

    solution to prompt1.js

--- Fundamentals/prompt1.js
+++ Fundamentals/prompt1.js
@@ -1,10 +1,10 @@
 // Write a function that takes a range of numbers from 1 to N. Sum every number divisible by M. Both N and M must be a number that is greater than 1.
-
+//
 // Example
-
+//
 // Input:
 // N = 8, M = 3
-
+//
 // Output:
 // 3 and 6 are divisible by 3. 3 + 6 = 9
 
@@ -14,22 +14,33 @@
 // Output:
 // 7 and 14 are divisible by 7. 7 + 14 = 21
 
+// function Dicks(n, m) {
+//   var sum = 0
+//   for (var i = 1; i <= n; i++) {
+//     if (i % m == 0) {
+//       sum += i
+//     }
+//   }
+//   return sum
+// }
 
-function range (n, m) {
-  let total = 0;
 
-  // loop to see how many numbers there are. with the num.length
 
-  for (let i = 0; i < n.length; i++) {
-    console.log('HELLO')
+function range(n, m) {
+  var sum = 0
+  for (var i = 1; i <= n; i++) {
+    if (i % m == 0) {
+      sum += i
+    }
   }
-
-
-  return total
+  return sum
 }
-
+range(8, 3)
 range(8, 3)
 // result should be 9
+
+// loop to see how many numbers there are. with the num.length
+
 
 // n is total (8)
 // how many numbers (not leftover so moduler) can be divisible?
